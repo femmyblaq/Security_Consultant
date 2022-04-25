14bf314d5324aa81a6d2b5533c8df2da332c178b
femmyblaq <femmyblaq@gmail.com>
2022-04-25 09:47:43 +0100

    complete

--- javascript/index.js
+++ javascript/index.js
@@ -90,3 +90,14 @@ if (window.PointerEvent) {
   window.addEventListener("mousemove", gestureMove);
   window.addEventListener("mouseup", gestureEnd);
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
